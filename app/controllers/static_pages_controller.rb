3b03d867add7f918b356107c686449414178b000
Nguyen Hong Viet <nguyen.hong.viet@framgia.com>
2015-01-28 10:47:19 +0700

    Fix Homepage, Following/follower page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,9 @@ class StaticPagesController < ApplicationController
   	if logged_in?
 	  	@entry 		= current_user.entries.build
 	  	@feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
-	end
+    else
+      @feed_items = Entry.all.paginate(page: params[:page], per_page: 10)
+	  end
   end
 
   def help
